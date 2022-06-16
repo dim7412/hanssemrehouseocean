02292406c87d53697222a5b8705bef6043e10e90
dim7412 <dim7412@nate.com>
2022-06-16 11:59:51 +0900

    220616

---
+++ js/desigin.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+  $("header").load("../include/header.html");  // 원하는 파일 경로를 삽입하면 된다
+  $("footer").load("../include/footer.html");  // 추가 인클루드를 원할 경우 이런식으로 추가하면 된다
+});
+
